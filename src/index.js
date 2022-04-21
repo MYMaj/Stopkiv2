c1798e9dd84da2a93c756161ece2ce7c15ab0f35
Marek <73800803+MYMaj@users.noreply.github.com>
2022-04-21 17:11:15 +0200

    Update full poprawione literówki rozpoznawanie znaków polski i ua numer rozdzielany

--- src/index.js
+++ src/index.js
@@ -45,12 +45,25 @@ $(".controls > .save").click(function () {
 $("#inputs input").keyup(function () {
   var input = $(this).attr("id");
   var val = $(this).val();
+
   $("." + input).html(val);
 
   if (input === "name" || "surname") {
     const name = $("#name").val();
     const surname = $("#surname").val();
-    const email = `${name}.${surname}`.toLocaleLowerCase("en");
+    let emailTemp = name + "." + surname;
+    // var reg = new RegExp("[ąćężźłóń]+", "i");
+    emailTemp = emailTemp.replace(/ą/i, "a");
+    emailTemp = emailTemp.replace(/ć/i, "c");
+    emailTemp = emailTemp.replace(/ę/i, "e");
+    emailTemp = emailTemp.replace(/ż/i, "z");
+    emailTemp = emailTemp.replace(/ź/i, "z");
+    emailTemp = emailTemp.replace(/ł/i, "l");
+    emailTemp = emailTemp.replace(/ó/i, "o");
+    emailTemp = emailTemp.replace(/n/i, "n");
+
+    const email = emailTemp.toLocaleLowerCase("en-US");
+    // const email = `${name}.${surname}`.toLocaleLowerCase("en-US");
 
     $(".email").each(function () {
       const domain = $(this).data("domain") || "";
@@ -58,6 +71,44 @@ $("#inputs input").keyup(function () {
       $(this).attr("href", `mailto:${email}@${domain}`);
     });
   }
+
+  if (input === "phone") {
+    $(".phone").each(function () {
+      var optionsPhoneCode = document.getElementById("code");
+      var selectPhoneCode =
+        optionsPhoneCode.options[optionsPhoneCode.selectedIndex].value;
+      console.log(selectPhoneCode);
+      var phone = $("#phone").val();
+      var PhoneTemp = phone;
+      PhoneTemp = PhoneTemp.trim();
+      PhoneTemp = PhoneTemp.replace(/\s/g, "");
+      if (selectPhoneCode === "+48" && PhoneTemp.length >= 3) {
+        let slicePL1 = PhoneTemp.substr(0, 3);
+        let slicePL2 = PhoneTemp.substr(3, 3);
+        let slicePL3 = PhoneTemp.substr(6, 3);
+        let slicePL = slicePL1 + " " + slicePL2 + " " + slicePL3;
+
+        PhoneTemp = slicePL;
+        console.log(slicePL);
+      }
+      if (selectPhoneCode === "+38" && PhoneTemp.length >= 3) {
+        let slicePL1 = PhoneTemp.substr(0, 3);
+        let slicePL2 = PhoneTemp.substr(3, 3);
+        let slicePL3 = PhoneTemp.substr(6, 2);
+        let slicePL4 = PhoneTemp.substr(8, 2);
+        let slicePL =
+          slicePL1 + " " + slicePL2 + " " + slicePL3 + " " + slicePL4;
+
+        PhoneTemp = slicePL;
+        console.log(slicePL);
+      }
+      phone = PhoneTemp;
+      let codePhone = selectPhoneCode + " " + phone;
+      //console.log(codePhone);
+      $(this).text(codePhone);
+    });
+  }
+
   $(".controls > a").hide();
   $(".controls > .save").show();
 });
