ae09d825633714b723312300106a088295a31703
Marek <73800803+MYMaj@users.noreply.github.com>
2022-07-04 17:18:59 +0200

    Poprawione zdjęcie ggo i foreign. JEdna tablcia Dodane sortowanie do tablicy Tablica połączona w jedną Bład zdjęc w outlook 2016 usuniety zdjęcia skalowanie

--- src/index.js
+++ src/index.js
@@ -3,168 +3,210 @@ import "./styles.css";
 // **********************************************************************************
 //        ARRAYS
 // Aray with all postitions in polish
-const array_position = [
+
+var array_position = [
   {
     text: "Doradca ds. Innowacji",
-    value: "Doradca ds. Innowacji"
+    value: "Doradca ds. Innowacji",
+    texteng: "Inovation Advisor"
   },
   {
     text: "Koordynator",
-    value: "Koordynator"
+    value: "Koordynator",
+    texteng: "Staff Coordinator"
   },
-  { text: "Kierownik Biura", value: "Kierownik Biura" },
+  {
+    text: "Kierownik Biura",
+    value: "Kierownik Biura",
+    texteng: "Office Manager"
+  },
   {
     text: "Starszy Kierownik ds. Floty i Transportu ",
-    value: "Starszy Kierownik ds. Floty i Transportu "
+    value: "Starszy Kierownik ds. Floty i Transportu ",
+    texteng: "Senior Fleet & Logistics Manager"
   },
   {
     text: "Kierownik ds. Zakwaterowania ",
-    value: "Kierownik ds. Zakwaterowania "
+    value: "Kierownik ds. Zakwaterowania ",
+    texteng: "Accommodation Manager"
   },
   {
     text: "Starszy Specjalista ds. Zakwaterowania ",
-    value: "Starszy Specjalista ds. Zakwaterowania "
+    value: "Starszy Specjalista ds. Zakwaterowania ",
+    texteng: "Senior Accommodation Specialist"
   },
   {
     text: "Specjalista ds. Administracji Personalnej ",
-    value: "Specjalista ds. Administracji Personalnej "
+    value: "Specjalista ds. Administracji Personalnej ",
+    texteng: "Personnel Administration Specialist"
   },
   {
     text: "Kierownik Działu Administracji Personalnej ",
-    value: "Kierownik Działu Administracji Personalnej "
+    value: "Kierownik Działu Administracji Personalnej ",
+    texteng: "Personnel Administration Manager"
   },
-  { text: "Recepcjonista ", value: "Recepcjonista " },
+  {
+    text: "Recepcjonista ",
+    value: "Recepcjonista ",
+    texteng: "Front Desk Receptionist"
+  },
   {
     text: "Specjalista ds. Controllingu ",
-    value: "Specjalista ds. Controllingu "
+    value: "Specjalista ds. Controllingu ",
+    texteng: "Controlling Specialist"
   },
-  { text: "Kierownik ds. Finansów ", value: "Kierownik ds. Finansów " },
+  {
+    text: "Kierownik ds. Finansów ",
+    value: "Kierownik ds. Finansów ",
+    texteng: "Finance Manager"
+  },
   {
     text: "Kierownik ds. Rozwoju Biznesu ",
-    value: "Kierownik ds. Rozwoju Biznesu "
+    value: "Kierownik ds. Rozwoju Biznesu ",
+    texteng: "Business Development Manager"
   },
-  { text: "Kierownik ds. Innowacji ", value: "Kierownik ds. Innowacji " },
+  {
+    text: "Kierownik ds. Innowacji ",
+    value: "Kierownik ds. Innowacji ",
+    texteng: "Innovation Manager"
+  },
   {
     text: "Młodszy Specjalista ds. Innowacji ",
-    value: "Młodszy Specjalista ds. Innowacji "
+    value: "Młodszy Specjalista ds. Innowacji ",
+    texteng: "Junior Innovation Specialist"
   },
-  { text: "Specjalista ds. IT ", value: "Specjalista ds. IT " },
+  {
+    text: "Specjalista ds. IT ",
+    value: "Specjalista ds. IT ",
+    texteng: "IT Specialist"
+  },
 
-  { text: "Starszy Programista ", value: "Starszy Programista " },
+  {
+    text: "Starszy Programista ",
+    value: "Starszy Programista ",
+    texteng: "Senior Software Developer"
+  },
   {
     text: "Specjalista ds. Kadr i Płac ",
-    value: "Specjalista ds. Kadr i Płac "
+    value: "Specjalista ds. Kadr i Płac ",
+    texteng: "HR & Payroll Specialist"
   },
   {
     text: "Starszy Specjalista ds. Kadr i Płac ",
-    value: "Starszy Specjalista ds. Kadr i Płac "
+    value: "Starszy Specjalista ds. Kadr i Płac ",
+    texteng: "Senior HR & Payroll Specialist"
   },
   {
     text: "Kierownik Działu Kadr i Płac ",
-    value: "Kierownik Działu Kadr i Płac "
+    value: "Kierownik Działu Kadr i Płac ",
+    texteng: "HR & Payroll Manager"
   },
   {
     text: "Specjalista ds. Zatrudnienia ",
-    value: "Specjalista ds. Zatrudnienia "
+    value: "Specjalista ds. Zatrudnienia ",
+    texteng: "Employment Specialist"
   },
   {
     text: "Specjalista ds. Legalizacji ",
-    value: "Specjalista ds. Legalizacji "
+    value: "Specjalista ds. Legalizacji ",
+    texteng: "Legalization Specialist"
   },
   {
     text: "Starszy Specjalista ds. Legalizacji ",
-    value: "Starszy Specjalista ds. Legalizacji "
+    value: "Starszy Specjalista ds. Legalizacji ",
+    texteng: "Senior Legalization Specialist"
   },
   {
     text: "Koordynator Personelu ",
-    value: "Koordynator Personelu "
+    value: "Koordynator Personelu ",
+    texteng: "Staff Coordinator"
   },
   {
     text: "Starszy Koordynator Personelu ",
-    value: "Starszy Koordynator Personelu "
+    value: "Starszy Koordynator Personelu ",
+    texteng: "Senior Staff Coordinator"
   },
   {
     text: "Starszy Kierownik Operacyjny ",
-    value: "Starszy Kierownik Operacyjny "
+    value: "Starszy Kierownik Operacyjny ",
+    texteng: "Senior Operations Manager"
   },
-  { text: "Specjalista ds. Rekrutacji ", value: "Specjalista ds. Rekrutacji " },
+  {
+    text: "Specjalista ds. Rekrutacji ",
+    value: "Specjalista ds. Rekrutacji ",
+    texteng: "Recruitment Specialist"
+  },
   {
     text: "Starszy Specjalista ds. Rekrutacji ",
-    value: "Starszy Specjalista ds. Rekrutacji "
+    value: "Starszy Specjalista ds. Rekrutacji ",
+    texteng: "Senior Recruitment Specialist"
   },
   {
     text: "Starszy Koordynator Personelu ",
-    value: "Starszy Koordynator Personelu "
+    value: "Starszy Koordynator Personelu ",
+    texteng: "Senior Staff Coordinator"
   },
-  { text: "Analityk Czasu Pracy ", value: "Analityk Czasu Pracy " },
-  { text: "Analityk Danych ", value: "Analityk Danych " },
+  {
+    text: "Analityk Czasu Pracy ",
+    value: "Analityk Czasu Pracy ",
+    texteng: "Working Time Analyst"
+  },
+  {
+    text: "Analityk Danych ",
+    value: "Analityk Danych ",
+    texteng: "Database Analyst"
+  },
   {
     text: "Starszy Prawnik | Specjalista ds. zgodności ",
-    value: "Starszy Prawnik | Specjalista ds. zgodności "
+    value: "Starszy Prawnik | Specjalista ds. zgodności ",
+    texteng: "Senior Lawyer | Compliance Officer"
   },
-  { text: "Kierownik ds. Rekrutacji ", value: "Kierownik ds. Rekrutacji " },
+  {
+    text: "Kierownik ds. Rekrutacji ",
+    value: "Kierownik ds. Rekrutacji ",
+    texteng: "Recruitment Manager"
+  },
 
-  { text: "Specjalista ds. Rekrutacji ", value: "Specjalista ds. Rekrutacji " },
+  {
+    text: "Specjalista ds. Rekrutacji ",
+    value: "Specjalista ds. Rekrutacji ",
+    texteng: "Recruitment Specialist"
+  },
   {
     text: "Starszy Specjalista ds. Rekrutacji ",
-    value: "Starszy Specjalista ds. Rekrutacji "
+    value: "Starszy Specjalista ds. Rekrutacji ",
+    texteng: "Senior Recruitment Specialist"
   },
   {
     text: "Specjalista ds. Marketingu Rekrutacyjnego ",
-    value: "Specjalista ds. Marketingu Rekrutacyjnego "
+    value: "Specjalista ds. Marketingu Rekrutacyjnego ",
+    texteng: "Recruitment Marketing Specialist"
   },
   {
     text: "Kierownik ds. Organizacji i Administracji ",
-    value: "Kierownik ds. Organizacji i Administracji "
+    value: "Kierownik ds. Organizacji i Administracji ",
+    texteng: "Organization and Administration Manager"
   },
-  { text: "HR Generalista ", value: "HR Generalista " },
-  { text: "Kierownik Projektu ", value: "Kierownik Projektu " },
-  { text: "Prezes Zarządu ", value: "Prezes Zarządu " },
-  { text: "Dyrektor Zarządzający ", value: "Dyrektor Zarządzający " }
-];
-
-// Aray with all postitions in english, should match array_position 1to1
-const array_positionENG = [
-  "Inovation Advisor",
-  "Staff Coordinator",
-  "Office Manager",
-  "Senior Fleet & Logistics Manager",
-  "Accommodation Manager",
-  "Senior Accommodation Specialist",
-  "Personnel Administration Specialist",
-  "Personnel Administration Manager",
-  "Front Desk Receptionist",
-  "Controlling Specialist",
-  "Finance Manager",
-  "Business Development Manager",
-  "Innovation Manager",
-  "Junior Innovation Specialist",
-  "IT Specialist",
-  "Senior Software Developer",
-  "HR & Payroll Specialist",
-  "Senior HR & Payroll Specialist",
-  "HR & Payroll Manager",
-  "Employment Specialist",
-  "Legalization Specialist",
-  "Senior Legalization Specialist",
-  "Staff Coordinator",
-  "Senior Staff Coordinator",
-  "Senior Operations Manager",
-  "Recruitment Specialist",
-  "Senior Recruitment Specialist",
-  "Senior Staff Coordinator",
-  "Working Time Analyst",
-  "Database Analyst",
-  "Senior Lawyer | Compliance Officer",
-  "Recruitment Manager",
-  "Recruitment Specialist",
-  "Senior Recruitment Specialist",
-  "Recruitment Marketing Specialist",
-  "Organization and Administration Manager",
-  "HR Generalist",
-  "Project Manager",
-  "Chief Executive Officer",
-  "Managing Director"
+  {
+    text: "HR Generalista ",
+    value: "HR Generalista ",
+    texteng: "HR Generalist"
+  },
+  {
+    text: "Kierownik Projektu ",
+    value: "Kierownik Projektu ",
+    texteng: "Project Manager"
+  },
+  {
+    text: "Prezes Zarządu ",
+    value: "Prezes Zarządu ",
+    texteng: "Chief Executive Officer"
+  },
+  {
+    text: "Dyrektor Zarządzający ",
+    value: "Dyrektor Zarządzający ",
+    texteng: "Managing Director"
+  }
 ];
 
 const $ = jQuery;
@@ -236,6 +278,7 @@ function selectText(elementId) {
 // Main Fucntion to wait until page is loaded
 $(document).ready(function () {
   // Fucntion button Copy to clipboard
+
   $(".controls > .select").click(function () {
     var signatureId = $(this).parent().data("sig");
     selectText(signatureId);
@@ -263,11 +306,12 @@ $(document).ready(function () {
     var indexPositionSelect = indexMatchingText(array_position, searchtext);
 
     $(".departament").each(function () {
-      $(".departament").text(array_positionENG[indexPositionSelect]);
+      $(".departament").text(array_position[indexPositionSelect].texteng);
     });
   });
 
   // Main function with assignig input to values
+  //************************************************************ */
   //Checking, regex,
   $("#inputs input").keyup(function (e) {
     var input = $(this).attr("id");
@@ -349,7 +393,7 @@ $(document).ready(function () {
         }
         phone = PhoneTemp;
         let codePhone = selectPhoneCode + " " + phone;
-        //console.log(codePhone);
+
         $(this).prop("href", "tel:" + codePhone);
         $(this).text(codePhone);
       });
@@ -358,20 +402,25 @@ $(document).ready(function () {
     $(".controls > a").hide();
     $(".controls > .save").show();
   });
-  /*// Function to resize unput width 
-  $("#inputs input").keyup(function () {
-    $(this).attr("size", $(this).val().length);
-  });
-*/
 
   //Generate option list form Array of positions
+
   const selectBox = document.querySelector("select");
 
+  //Sort Array position
+  array_position.sort((a, b) => a.value.localeCompare(b.value));
+  console.log(array_position);
   for (const o of array_position) {
-    const { text, value, selected } = o;
-    selectBox.options.add(new Option(text, value, selected, selected));
+    const { text, value, texteng, selected } = o;
+    selectBox.options.add(new Option(text, value, texteng, selected, selected));
   }
 });
+// END READY FUNCTIONS
+
+// **********************************************************************************
+//        Other functions
+
+// **********************************************************************************
 // Function to check for html tags in input
 function removeTags(html) {
   var oldHtml;
@@ -381,13 +430,14 @@ function removeTags(html) {
   } while (html !== oldHtml);
   return html.replace(/</g, "&lt;");
 }
-
+// **********************************************************************************
 // Function to display error massage set by place
 function errorMessage(messageERR) {
   var errorEN = document.getElementById("error1");
   errorEN.textContent = messageERR;
   errorEN.style.color = "red";
 }
+// **********************************************************************************
 // Function to block invalid chars in input
 $("#inputs input").keydown(function (e) {
   //Chceck for input invalid chars
@@ -397,6 +447,8 @@ $("#inputs input").keydown(function (e) {
   } else {
   }
 });
+
+// **********************************************************************************
 // Not Worknig. Function to sanitize paste event
 
 $("#name").bind("paste", function () {
@@ -405,7 +457,7 @@ $("#name").bind("paste", function () {
   setTimeout(function () {
     //get the value of the input text
     var val = $("#name").val();
-    console.log(val);
+
     //replace the special characters to ''
     var dataFull = val.replace(/[\d\\()"'.,/#@$% ]/gi, "");
 
@@ -413,14 +465,14 @@ $("#name").bind("paste", function () {
     $("#name").val(dataFull);
   });
 });
-
+// **********************************************************************************
 $("#surname").bind("paste", function () {
   //var input = $(this).attr("id");
 
   setTimeout(function () {
     //get the value of the input text
     var val = $("#surname").val();
-    console.log(val);
+
     //replace the special characters to ''
     var dataFull = val.replace(/[\d\\()"'.,/#@$% ]/gi, "");
 
@@ -428,14 +480,14 @@ $("#surname").bind("paste", function () {
     $("#surname").val(dataFull);
   });
 });
-
+// **********************************************************************************
 $("#phone").bind("paste", function () {
   //var input = $(this).attr("id");
 
   setTimeout(function () {
     //get the value of the input text
     var val = $("#phone").val();
-    console.log(val);
+
     //replace the special characters to ''
     var dataFull = val.replace(/[\D]/gi, "");
 
@@ -443,15 +495,15 @@ $("#phone").bind("paste", function () {
     $("#phone").val(dataFull);
   });
 });
+
+// **********************************************************************************
 // Find what index select position have
 
 function indexMatchingText(ele, text) {
   const result3 = ele.length;
 
   for (var i = 0; i < result3; i++) {
-    console.log(ele[i]);
     if (ele[i].text === text) {
-      console.log(text);
       return i;
     }
   }
